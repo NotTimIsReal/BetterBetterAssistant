deb9cc253903a6f559fa4d67c8ff58d933f8e6a3
NotTimIsReal <alastairrintumathew@gmail.com>
2021-07-14 15:30:31 +1000

    e

--- commands/say.js
+++ commands/say.js
@@ -4,8 +4,8 @@ module.exports={
     description:'ur mum',
     execute(client, message, args){
         if(!args[0]) return message.channel.reply('BRuh')
-        args.slice(0).join(" ")
-        message.channel.send(args[0])
+        msg=args.slice(0).join(" ")
+        message.channel.send(msg)
     }
 
 }
